b8e1b04601982bbeeff619f2a418d5f554488ec5
Luciano Roberto <lucianoroberto10@live.com>
2018-06-20 16:26:29 -0300

    Trabalho Lab SO

--- Luciano1.sh
+++ Luciano1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-find . -mtime -1 -exec cp {} 
+if [find . -mtime -1]  then
+
+git config --global user.name "Luciano"
+git config --global user.email "lucianoroberto10@live.com"
+git add *
+git commit -m "mensagem"
+git push
+
+fi
 
 
