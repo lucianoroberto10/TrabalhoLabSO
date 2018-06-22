46825a470f23bc71cdde7910f40efe978cc5c739
Luciano Roberto <lucianoroberto10@live.com>
2018-06-22 20:38:19 -0300

    Trabalho de Lab SO

--- Luciano1.sh
+++ Luciano1.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+arq=$1
+
+for arq in $(ls .)
+
 if [find . -mtime -1]  then
 
-git config --global user.name "Luciano"
-git config --global user.email "lucianoroberto10@live.com"
+git config --global user.name "ExemploNome"
+git config --global user.email "Exemplo@gmail.com"
 git add *
 git commit -m "mensagem"
 git push
 
 fi
 
+done
 
